01567a7f98e43259ac112a00a274a04a325bb16c
Anderson Alves <anderson.dmc07@gmail.com>
2024-01-10 16:31:43 -0300

    Concluido

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // TODO: Adicionar um construtor para a classe Nokia que chame o construtor da classe base
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Inicializar outras propriedades específicas da classe Nokia, se houver
+        }
+
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            // TODO: Implementar a lógica para instalar o aplicativo em um dispositivo Nokia
+            Console.WriteLine($"Instalando {nomeApp} no dispositivo Nokia...");
+        }
     }
 }
