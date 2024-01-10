8c58fe5e3fed4ec8ecf5e39e219b90956fa3aac3
Anderson Alves <anderson.dmc07@gmail.com>
2024-01-10 16:35:26 -0300

    Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; } // Propriedade Modelo
+        public string Imei { get; set; } // Propriedade Imei
+        public int Memoria { get; set; } // Propriedade Memoria
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
