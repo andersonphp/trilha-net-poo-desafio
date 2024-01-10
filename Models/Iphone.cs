95138a7e564284ae831f933de504c35a3c7dec08
Anderson Alves <anderson.dmc07@gmail.com>
2024-01-10 16:20:57 -0300

    Concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // TODO: Adicionar um construtor para a classe Iphone que chame o construtor da classe base
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Inicializar outras propriedades específicas da classe Iphone, se houver
+        }
+
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            // TODO: Implementar a lógica para instalar o aplicativo em um dispositivo Iphone
+            Console.WriteLine($"Instalando {nomeApp} no dispositivo Iphone...");
+        }
     }
 }
